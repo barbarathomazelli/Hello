40dd465785a39ec07002cbdc11c37a547ddc360c
Barbara <barbarathomazelli@gmail.com>
2021-12-02 21:21:35 -0300

    hello

--- src/hello.java
+++ src/hello.java
@@ -3,6 +3,17 @@ package com.java;
 public class hello {
 
     public static void main (String[]args){
+
+        int idade = 10;
+        double preco =10.0;
+        char sexo = 'F' ;
+        boolean temfilhos = true;
+
          System.out.println("Hello");
+         System.out.println(idade);
+        System.out.println(preco);
+        System.out.println(sexo);
+        System.out.println(temfilhos);
+
     }
 }
